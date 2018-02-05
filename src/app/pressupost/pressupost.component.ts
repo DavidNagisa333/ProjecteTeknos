95a42c4f610b46ff6f1027783335307400e9c413
ITE-SSALES\ssales <salesvila67@gmail.com>
2018-02-05 10:20:19 +0100

    no message

--- src/app/pressupost/pressupost.component.ts
+++ src/app/pressupost/pressupost.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pressupost',
   templateUrl: './pressupost.component.html',
-  styleUrls: ['./pressupost.component.css']
 })
 export class PressupostComponent implements OnInit {
 
